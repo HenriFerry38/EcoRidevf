77593c2cfe8ca6046b4c3a8a35a6a248a38c3f7c
Henri FERRY <henricosmique@gmail.com>
2025-10-24 12:50:34 +0200

    Création et routages des pages Acceuil,Covoiturages,Connexion et Contact

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,6 +2,9 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/Covoiturages", "Covoiturages", "/pages/covoiturages.html"),
+    new Route("/Connexion", "Connexion", "/pages/connexion.html"),
+    new Route("/Contact", "Contact", "/pages/contact.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
